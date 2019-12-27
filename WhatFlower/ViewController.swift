8031e5844638d69caba671255e1e0f2a1ce2675e
Glad <gladpoenaru@icloud.com>
2019-12-26 23:59:28 -0700

    Recognize the flower

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -5,10 +5,11 @@
 //  Created by Glad Poenaru on 2019-12-26.
 //  Copyright © 2019 Glad Poenaru. All rights reserved.
 //
-
+// Best Project Flower
 import UIKit
+import Vision
+import CoreML
 
-import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -17,7 +18,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     override func viewDidLoad() {
         
-        //change in code
         imagePicker.delegate = self
         imagePicker.sourceType = .camera
         imagePicker.allowsEditing = true
@@ -27,8 +27,30 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let userPickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
             imageView.image = userPickedImage
+            guard let ciflowerImage = CIImage(image:userPickedImage) else { fatalError("Can not convert image to CIImage")}
+        detectFlower(flowerImage: ciflowerImage)
         }
         imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detectFlower(flowerImage: CIImage) {
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {fatalError("Loading CoreML model failer")}
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {fatalError("Model failed to process results")}
+            print(results)
+            if let firstResult = results.first {
+                self.navigationItem.title = firstResult.identifier
+            } else { self.navigationItem.title = "I dont know what flower it is"}
+            
+       
+        }
+        let handler = VNImageRequestHandler(ciImage: flowerImage)
+        do {
+            try handler.perform([request])
+        } catch
+        { print("Error with the handler, \(error)")}
+        
+        
     }
     
 
